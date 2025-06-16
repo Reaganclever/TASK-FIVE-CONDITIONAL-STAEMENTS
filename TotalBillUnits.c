c56072eb632f74c850285ba8d554e2102d7a31ae
reaganclever <kasulereagan86@gmail.com>
2025-06-16 19:28:21 +0300

    TFWWCS5-7 COMMIT#2 ON BRANCH#3

--- TotalBillUnits.c
+++ TotalBillUnits.c
@@ -6,18 +6,18 @@ while(1){
         double totalunits1 , totalunits2 , oneUnitCost = 1000 ;
 
        if(units < 200){
-        totalunits1 = units * 0.6;
+        totalunits1 = units * 0.3;
       printf("TOTAL UNITS LESS THAN 200: ");
       double totalBill_1 = oneUnitCost * totalunits1;
-      printf("NEW TOTAL BILL = %.2lf" , totalBill_1);
+      printf("NEWEST TOTAL BILL = %.2lf" , totalBill_1);
        break;
        }
 
        else if(units >= 200){
-        totalunits2 = units * 0.85;
-       printf("NEW TOTAL UNITS GREATER OR EQUAL TO 200: ");
+        totalunits2 = units * 0.45;
+       printf("NEWEST TOTAL UNITS GREATER OR EQUAL TO 200: ");
         double totalBill_2 = oneUnitCost * totalunits2;
-        printf("NEW TOTAL BILL  = %.2lf" , totalBill_2);
+        printf("NEWEST TOTAL BILL  = %.2lf" , totalBill_2);
         break;
 
        }
