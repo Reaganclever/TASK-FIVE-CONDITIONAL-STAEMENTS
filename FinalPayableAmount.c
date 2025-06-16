aac63411f8972452ecab8ed2f61678d2afe9800d
reaganclever <kasulereagan86@gmail.com>
2025-06-16 20:34:08 +0300

    TFWWCS5-9 COMMIT#1 ON BRANCH#4

--- FinalPayableAmount.c
+++ FinalPayableAmount.c
@@ -7,22 +7,22 @@ double computeFinalPayableAmount(int membershipDuration , double totalFee){
         double discountAmount1_3 , discountAmount_EqualandAbove4 , discountTotalGymPrice1_3 , discountTotalgymPrice_EqualandAbove4;
 
         if(membershipDuration >= 1 && membershipDuration <= 3){
-            discountAmount1_3 = totalFee * 0.05;
+            discountAmount1_3 = totalFee * 0.08;
             discountTotalGymPrice1_3 = totalFee - discountAmount1_3;
-            printf("THE DISCOUNT AMOUNT IS %.0lf\n" , discountAmount1_3);
-            printf("THE FINAL PAYABLE AMOUNT IS %.2lf\n" , discountTotalGymPrice1_3);
+            printf("THE NEW DISCOUNT AMOUNT IS %.0lf\n" , discountAmount1_3);
+            printf("THE NEW FINAL PAYABLE AMOUNT IS %.2lf\n" , discountTotalGymPrice1_3);
             break;
 
         }
         else if(membershipDuration >=4){
-             discountAmount_EqualandAbove4 = totalFee * 0.1;
+             discountAmount_EqualandAbove4 = totalFee * 0.12;
             discountTotalgymPrice_EqualandAbove4 = totalFee - discountAmount_EqualandAbove4;
-            printf("THE DISCOUNT AMOUNT 4+ IS %.0lf\n" , discountAmount_EqualandAbove4);
-            printf("THE FINAL PAYABLE AMOUNT 4+ IS %.2lf\n" , discountTotalgymPrice_EqualandAbove4);
+            printf("THE NEW DISCOUNT AMOUNT 4+ IS %.0lf\n" , discountAmount_EqualandAbove4);
+            printf("THE NEW FINAL PAYABLE AMOUNT 4+ IS %.2lf\n" , discountTotalgymPrice_EqualandAbove4);
             break;
         }
         else{
-            printf("INVALID MEMBERSHIP DURATION\n");
+            printf(" SO INVALID MEMBERSHIP DURATION\n");
             break;
         }
         }
