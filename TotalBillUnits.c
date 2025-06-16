df3444570ad39a1df75180526687cbbc6096965d
reaganclever <kasulereagan86@gmail.com>
2025-06-16 19:23:56 +0300

    Initial

---
+++ TotalBillUnits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "TaskFiveProject.h"
+
+double calculateTotalBills(double units){
+while(1){
+        double totalunits1 , totalunits2 , oneUnitCost = 1000 ;
+
+       if(units < 200){
+        totalunits1 = units * 0.5;
+      printf("TOTAL UNITS LESS THAN 200: ");
+      double totalBill_1 = oneUnitCost * totalunits1;
+      printf("TOTAL BILL = %.2lf" , totalBill_1);
+       break;
+       }
+
+       else if(units >= 200){
+        totalunits2 = units * 0.75;
+       printf("TOTAL UNITS GREATER OR EQUAL TO 200: ");
+        double totalBill_2 = oneUnitCost * totalunits2;
+        printf("TOTAL BILL  = %.2lf" , totalBill_2);
+        break;
+
+       }
+
+}
+
+}
+
+
