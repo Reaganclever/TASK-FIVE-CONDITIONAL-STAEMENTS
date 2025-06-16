96bc09f071f7560389c93874c1be89ec55399ab6
reaganclever <kasulereagan86@gmail.com>
2025-06-16 20:41:30 +0300

    TFWWCS5-10 COMMIT#1 ON BRANCH#5

--- TotalCost.c
+++ TotalCost.c
@@ -9,14 +9,14 @@ while(1){
         totalCost1 = priceperunit * quantity;
 
        if(quantity >= 10){
-        double discountTotal = totalCost1 * 0.15;
+        double discountTotal = totalCost1 * 0.24;
         double totalcostWithdiscount = totalCost1 - discountTotal;
-        printf("DISCOUNT TOTAL IS %.2lf\n" , discountTotal);
-        printf("THE TOTAL COST AFTER DISCOUNT IS %.2lf\n" , totalcostWithdiscount);
+        printf("NEW DISCOUNT TOTAL IS %.2lf\n" , discountTotal);
+        printf("THE NEW TOTAL COST AFTER DISCOUNT IS %.2lf\n" , totalcostWithdiscount);
        }
        else{
         printf("NO DISCOUNT");
-        printf("\nTHE TOTAL COST WITH NO DISCOUNT IS %.2lf\n" , totalCost1);
+        printf("\nTHE  OLD TOTAL COST WITH NO DISCOUNT IS %.2lf\n" , totalCost1);
         break;
        }
         }
